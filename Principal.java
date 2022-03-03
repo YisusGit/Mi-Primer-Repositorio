c293135265b8dc1b468c235162cc5ba099738778
Jesus <jesusrr2000@gmail.com>
2022-03-03 02:09:38 +0100

    Prueba2

--- Principal.java
+++ Principal.java
@@ -2,5 +2,9 @@ public class Principal {
      public static void main(String[] args) {
         String x = "Hola mundo";
         System.out.println(x);
+        for(int i = 0; i < 9; i++ ){
+            System.out.println(i);
+        }
+
     }
 }
